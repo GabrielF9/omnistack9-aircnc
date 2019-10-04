0cc5d414035240fafda1642e1567c342a208e778
GabrielF9 <gabriel.ferreira@dcomp.ufs.br>
2019-10-04 15:25:51 -0300

    feat: frontend created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongo-link', {
+mongoose.connect('mongo_url', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -15,8 +17,9 @@ mongoose.connect('mongo-link', {
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
